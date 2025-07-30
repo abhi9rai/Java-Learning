e14fc7d508184f5c48bb79a72e8e94ea85c5f7c4
Abhinav Rai <abhinavrai9454@gmail.com>
2025-07-30 23:43:47 +0530

    Update

---
+++ src/basics/literals_04.java
@@ -0,0 +1,25 @@
+package basics;
+
+public class literals_04 {
+    public static void main(String[] args) {
+        byte age = 34;
+        short age1 = 44;
+        int age2 = 56;
+        long ageDino = 678877383939739L;
+        char ch = 'A';
+        float f1 = 3.4f;
+        double d1 = 4.6664;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(ageDino);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(a);
+        String str = "Abhinav Rai";
+        System.out.println(str);
+
+    }
+}
