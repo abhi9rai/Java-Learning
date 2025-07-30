20ab347060ce90d2858db4a9534c203b3bda9999
Abhinav Rai <abhinavrai9454@gmail.com>
2025-07-30 23:23:28 +0530

    Updated

---
+++ src/basics/addthreenumbers.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class addthreenumbers {
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        int c=7;
+        int d=a+b+c;
+        System.out.println("The required addition of three numbers is : " +d );
+    }
+}
