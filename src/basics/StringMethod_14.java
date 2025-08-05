bf672060220ea62c8e95e7eb6745a4b86785bce8
abhi9rai <abhinavrai9453@gmail.com>
2025-08-05 09:30:47 +0530

    IF-ELSE

---
+++ src/basics/StringMethod_14.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class StringMethod_14 {
+    public static void main(String[] args) {
+        String a = "Abhinav Rai";
+        //System.out.println(a.length());
+        //System.out.println(a.toLowerCase());
+        //System.out.println(a.toUpperCase());
+
+        //String non = "   Abhinav    ";
+        //System.out.println(non.trim());
+
+        //System.out.println(a.substring(2));
+        //System.out.println(a.substring(2,4));
+
+        String name = "Harry";
+        //System.out.println(name.replace('r','p'));
+
+        //System.out.println(name.startsWith("Ha"));
+        //System.out.println(name.endsWith("ry"));
+
+        System.out.println(name.charAt(0));
+        System.out.println(name.indexOf("r"));
+        System.out.println(name.indexOf("r",3));
+    }
+}
---
+++ src/basics/Strings_13.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Strings_13 {
+    public static void main(String[] args) {
+        //String name = new String("Abhinav Rai");
+        String name = "Abhinav Rai";
+        System.out.println(name);
+    }
+}
