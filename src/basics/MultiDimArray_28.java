25f6dc6d6a6071a055adc6f0022d1e30ecc5bf44
abhi9rai <abhinavrai9453@gmail.com>
2025-08-12 21:37:36 +0530

    MultiDim

---
+++ src/basics/MultiDimArray_28.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class MultiDimArray_28 {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        int [][] flats = new int[2][3];
+        flats [0][0]=101;
+        flats [0][1]=102;
+        flats [0][2]=103;
+        flats [1][0]=201;
+        flats [1][1]=202;
+        flats [1][2]=203;
+        for (int i=0; i< flats.length;i++){
+            for (int j=0; j< flats[i].length;j++){
+                System.out.println(flats[i][j]);
+                System.out.println(" ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
