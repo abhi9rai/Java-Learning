898f3319df32273d064eba4731e07513848ade85
abhi9rai <abhinavrai9453@gmail.com>
2025-08-21 19:34:17 +0530

    Varargs

---
+++ src/oop/varargs_33.java
@@ -0,0 +1,25 @@
+package oop;
+
+public class varargs_33 {
+//    static int sum(int a, int b){
+//        return a+b;
+//    }
+//    static int sum(int a, int b, int c){
+//        return a+b+c;
+//    }
+    static int sum(int ...arr){
+//        Available as int [] arr;
+        int result=0;
+        for(int a : arr){
+            result+=a;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Varargs Tutorial");
+        System.out.println("The sum of 4 and 5 is: "+ sum(4,5));
+        System.out.println("The sum of 4,3 and 5 is: "+ sum(4,3,5));
+        System.out.println("The sum of 4,3,1 and 5 is: "+ sum(4,1,3,5));
+
+    }
+}
