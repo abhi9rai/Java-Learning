5f8b6d727e6e9b4152b7ec0664aa4b4e83f71b82
abhi9rai <abhinavrai9453@gmail.com>
2025-09-15 20:23:12 +0530

    Inheritance

---
+++ src/oop/CH10_Inheritance_45.java
@@ -0,0 +1,44 @@
+package oop;
+
+class Base{
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        System.out.println("I am in base & setting x now");
+        this.x = x;
+    }
+
+    public void printMe(){
+        System.out.println("I am a constructor");
+    }
+}
+
+class Derived extends Base{
+    int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+public class CH10_Inheritance_45 {
+    public static void main(String[] args) {
+//        creating object of base class
+        Base b = new Base();
+        b.setX(4);
+        System.out.println(b.getX());
+
+//        creating object of derived class
+        Derived d = new Derived();
+        d.setY(43);
+        System.out.println(d.getY());
+    }
+}
