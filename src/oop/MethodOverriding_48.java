04cf9bc1c16af11c62d2519119cb516195c90c12
abhi9rai <abhinavrai9453@gmail.com>
2025-09-15 23:13:46 +0530

    MethodOverriding

---
+++ src/oop/MethodOverriding_48.java
@@ -0,0 +1,29 @@
+package oop;
+
+class A{
+    public int abhinav(){
+        return 4;
+    }
+    public void meth2(){
+        System.out.println("I am method 2 of class A");
+    }
+}
+class B extends A{
+    @Override
+    public void meth2(){
+        System.out.println("I am method 3 of class B");
+    }
+    public void meth3(){
+        System.out.println("I am method 3 of class B");
+    }
+}
+
+public class MethodOverriding_48 {
+    public static void main(String[] args) {
+        A a =new A();
+        a.meth2();
+
+        B b = new B();
+        b.meth2();
+    }
+}
