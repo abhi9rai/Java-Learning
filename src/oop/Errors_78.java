dc3a17399244367432d8803681cc7394e982ca66
abhi9rai <abhinavrai9453@gmail.com>
2025-09-20 11:03:23 +0530

    Errors

---
+++ src/oop/Errors_78.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Errors_78 {
+    public static void main(String[] args) {
+        int a= 5;
+//        int b= 5 --> syntax error
+//        c= 5 --> syntax error
+//        user supplies 's'+4 --> runtime error
+        System.out.println(a);
+    }
+}
