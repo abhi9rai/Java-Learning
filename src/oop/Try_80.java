8008c221fffac2bb9f0b9e8bda63cb6d15a574c5
abhi9rai <abhinavrai9453@gmail.com>
2025-09-20 11:39:04 +0530

    Try

---
+++ src/oop/Try_80.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class Try_80 {
+    public static void main(String[] args) {
+        int a=600;
+        int b=0;
+
+//        Without Try
+
+//        int c=a/b;
+//        System.out.println("The result is : " +c);
+
+//        With Try:
+
+        try{
+            int c=a/b;
+        } catch (RuntimeException e) {
+            System.out.println("We failed to divide. Reason ");
+            System.out.println(e);
+        }
+        System.out.println("End of the program");
+    }
+}
