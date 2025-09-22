c164c611934cf90ca5aa080897aa42e3aeb5de2b
abhi9rai <abhinavrai9453@gmail.com>
2025-09-22 19:46:01 +0530

    Javadoc

---
+++ src/oop/Javadoc_106.java
@@ -0,0 +1,19 @@
+package oop;
+
+/**
+ * This class is to demonstrate what javadoc is and how it is used in the java industry
+ * This is <i>Italic</i> word <p><This is a new paragraph/p>
+ * @author Abhinav Rai
+ * @version 0.1
+ * @since 2004
+ * @see <a href="https://download.java.net/java/GA/jdk14/docs/api/index.html" target ="_blank">Java Docs</a>
+ */
+
+public class Javadoc_106 {
+    public void add(int a, int b){
+        System.out.println("The sum is: "+ a+b);
+    }
+    public static void main(String[] args) {
+
+    }
+}
