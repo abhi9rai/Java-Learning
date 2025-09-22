5e53373f6253a9178ff4d0118b36d9038907f690
abhi9rai <abhinavrai9453@gmail.com>
2025-09-22 11:06:23 +0530

    Date_Class

---
+++ src/oop/Date_Class_97.java
@@ -0,0 +1,15 @@
+package oop;
+import java.util.Date;
+
+public class Date_Class_97 {
+    public static void main(String[] args) {
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(System.currentTimeMillis());
+        Date d= new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        System.out.println(d.getDate());
+        System.out.println(d.getSeconds());
+        System.out.println(d.getYear());
+    }
+}
